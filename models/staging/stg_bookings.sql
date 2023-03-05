c745a54818c4da8e482a466ede0062d93c6f2fbf
mgavrish1688@gmail.com <mgavrish1688@gmail.com>
2023-03-05 23:36:31 +0000

    removing duplicates from booking stg

--- models/staging/stg_bookings.sql
+++ models/staging/stg_bookings.sql
@@ -5,4 +5,5 @@ select
     created_at::date as created_at,
     start_date::date as start_date,
     end_date::date as end_date
-from {{ ref('raw_bookings') }}
+from {{ ref('raw_bookings') }} b
+qualify row_number() over (partition by b.id order by b.id desc) = 1
