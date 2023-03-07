38da3cebfd98bf19acc2d2988aaa297d4a6ea6cf
mgavrish1688@gmail.com <mgavrish1688@gmail.com>
2023-03-07 02:24:49 +0000

    updating models

--- models/marts/bookings_ft.sql
+++ models/marts/bookings_ft.sql
@@ -12,7 +12,8 @@ s.type,
 s.price_per_night,
 p.property_id,
 p.name as property_name,
-datediff(day, b.start_date, b.end_date) as stay_length
+datediff(day, b.start_date, b.end_date) as no_of_nights,
+no_of_nights * s.price_per_night as total_price
 from {{ ref('stg_bookings') }} b
 left join {{ ref('stg_users') }} u 
 on b.user_id = u.user_id
--- models/reports/rpt_metrics_by_property_type.sql
+++ models/reports/rpt_metrics_by_property_type.sql
@@ -2,5 +2,5 @@ select
 b.type as property_type,
 avg(b.price_per_night) as ave_price,
 avg(b.stay_length) as ave_stay_length
-from {{ ref('bookings_dt') }} b
+from {{ ref('bookings_ft') }} b
 group by 1
