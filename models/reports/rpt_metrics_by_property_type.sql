ebcda5b0302d143acb78ecd1a3705face60f1dc0
mgavrish1688@gmail.com <mgavrish1688@gmail.com>
2023-03-07 09:58:54 +0000

    updating dependencies to the duration name

--- models/reports/rpt_metrics_by_property_type.sql
+++ models/reports/rpt_metrics_by_property_type.sql
@@ -1,6 +1,6 @@
 select 
 b.type as property_type,
 avg(b.price_per_night) as ave_price,
-avg(b.no_of_nights) as ave_stay_length
+avg(b.duration) as ave_stay_length
 from {{ ref('bookings_ft') }} b
 group by 1
